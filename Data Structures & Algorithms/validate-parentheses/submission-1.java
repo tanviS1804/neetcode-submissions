0504c71b6242efe4415961003a402c5f95f82fc8
Tanvi Sonawane <tanvi.s1804@gmail.com>
2026-08-05 00:20:54 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put('{', '}');
+        map.put('[', ']');
+        map.put('(', ')');
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if (c != map.get(top)) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
