9bbb0efa80364fe79eaac53df6b1bbf0eb3f13fc
Tanvi Sonawane <tanvi.s1804@gmail.com>
2026-09-01 23:50:11 +0530

    Add: search-in-rotated-sorted-array-ii - submission-2

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        for (int num : nums) {
+            if (num == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
